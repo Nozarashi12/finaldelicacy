26a2186116787c1a1a2cbece3bfb9f39f968b5db
Nozarashi12 <adithyanadhi619@gmail.com>
2023-12-08 16:38:17 +0530

    hh

---
+++ delicacy.js
@@ -0,0 +1,87 @@
+
+function getRecipe() {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            document.querySelector(".random").innerHTML =
+                `<h2 class="title2">Random Recipe</h2>
+                <div class="rndimg">
+                    <button class="ibutton" onclick="ingredient()"><img class="recipe" src="${data.meals[0].strMealThumb}" ></button>
+                    <p class="rname">${data.meals[0].strMeal}</p>
+                </div>`;
+                // console.log(data);
+        })
+        .catch(error => {
+            console.log("Recipe not found");
+        });
+}
+
+getRecipe();
+
+function ingredient(){
+    const box=document.querySelector(".box")
+
+    box.style.visibility="visible"}
+
+document.querySelector(".search").addEventListener("input", getCat)
+
+function getCat() {
+    const catname = document.querySelector(".search").value;
+    const cat_URL = (`https://www.themealdb.com/api/json/v1/1/filter.php?c=${catname}`);
+    fetch(cat_URL)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector(".searchb4").innerHTML =
+                `<h2 class="b4meals">Popular Dishes made with ${catname}</h2>
+            <div class="mealimg">
+            <img class="meals" src="${data.meals[0].strMealThumb}">
+            <p class="lastp">${data.meals[0].strMeal}</p>
+            <img class="meals" src="${data.meals[1].strMealThumb}">
+            <p class="lastp">${data.meals[1].strMeal}</p>
+            <img class="meals" src="${data.meals[2].strMealThumb}">
+            <p class="lastp">${data.meals[2].strMeal}</p>
+            <img class="meals" src="${data.meals[3].strMealThumb}">
+            <p class="lastp">${data.meals[3].strMeal}</p>
+          </div>`;
+            // console.log(data);
+        })
+        .catch(error => {
+            console.log("Error fetching category");
+        });
+
+
+};
+
+
+getCat();
+
+function getingredient(){
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+    const mealname=document.querySelector(".box").innerHTML=
+    ` <h1>Ingredients</h1>
+    <div class="close" onclick="document.querySelector('.box').style.visibility='hidden'">&times;</div>
+</div>
+    <ol>
+    <li> ${data.meals[0].strIngredient1} ${data.meals[0].strMeasure1}</li>
+    <li>${data.meals[0].strIngredient2} ${data.meals[0].strMeasure2}</li>
+    <li> ${data.meals[0].strIngredient3} ${data.meals[0].strMeasure3}</li>
+    <li>${data.meals[0].strIngredient4} ${data.meals[0].strMeasure4}</li>
+    <li> ${data.meals[0].strIngredient5} ${data.meals[0].strMeasure5}</li>
+    <li>${data.meals[0].strIngredient6} ${data.meals[0].strMeasure6}</li>
+    <li> ${data.meals[0].strIngredient7} ${data.meals[0].strMeasure7}</li>
+    <li>${data.meals[0].strIngredient8} ${data.meals[0].strMeasure8}</li>
+    <li> ${data.meals[0].strIngredient9} ${data.meals[0].strMeasure9}</li>
+    <li>${data.meals[0].strIngredient10} ${data.meals[0].strMeasure10}</li>
+    <li> ${data.meals[0].strIngredient11} ${data.meals[0].strMeasure11}</li>
+    <li>${data.meals[0].strIngredient12} ${data.meals[0].strMeasure12}</li>
+     </ol>`    
+})
+}
+getingredient()
+
